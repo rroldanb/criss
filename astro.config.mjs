06bdd40032ad4c048e782d4837605af60075cb0f
Ruben Roldan <ruben.roldan.b@gmail.com>
2024-11-04 23:40:44 -0300

    deploy4

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://rroldanb.github.io',
-  base: '/criss'
+  site: 'https://rroldanb.github.io/criss',
 });
